1484ae220adc141362e93295949eabc66440bdce
colerusson <colerusson2@gmail.com>
2023-01-25 19:03:36 -0700

    fixed bugs and now it all works

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import argparse
-import numpy
-import os
-import math
 
 
 def main(number: int) -> int:
     # Write the code to sum up cubed numbers here.
     # Make sure that your terminal output matches the terminal output of the example given on the instructions.
     output = 0
-    starting_val = number
+    starting_val = number + 1
     for i in range(starting_val):
         cubed_val = i**3
-        while cubed_val > 10:
-            cubed_val = cubed_val // 10
-        if cubed_val % 2 == 0:
+        temp_val = cubed_val
+        while temp_val >= 10:
+            temp_val = temp_val // 10
+        if temp_val % 2 == 0:
             output += cubed_val
 
     return output
